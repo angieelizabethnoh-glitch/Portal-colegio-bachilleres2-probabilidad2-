883643cc89e27166da8fe6940914ceb87cbc133f
Angienoh <angieelizabethnoh@cobaqroo.edu.mx>
2026-03-23 22:39:24 -0500

    Add files via upload

--- angie-page/pagina.php
+++ angie-page/pagina.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title> Milagro Mexicano </title>
-    <link rel="stylesheet" href="css/Style2.css">
+    <title> Medidas de tendencia central</title>
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/popper.min.js"></script>
@@ -21,9 +21,10 @@
             <nav>
                 <ul style="list-style: none; display: flex; gap: 20px; justify-content: flex-start; padding-left: 30px;">
                     <li><a href="#home">Home</a></li>
-                    <li><a href="#movimientos">Movimientos</a></li>
-                    <li><a href="#protestas">Protestas</a></li>
-                    <li><a href="#gobierno">Gobierno</a></li>
+                    <li><a href="#media">Media</a></li>
+                    <li><a href="#mediana">Mediana</a></li>
+                    <li><a href="#moda">Moda</a></li>
+                    <li><a href="#referencias">Referencias</a></li>
                 </ul>
             </nav>
         </div>
@@ -31,174 +32,125 @@
         <br>
         <br>
         <br>
-        <div class="titulin">
-            <h1 style="text-align: left;">Milagro Mexicano</h1> <br>
-            <h5>Esta página está dedicada al periodo del modelo económico <br>
-                de desarrollo estabilizador, conocido como &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <spam class="milagrito">MILAGRO MEXICANO</spam> <br>
-                Abrió una etapa de esperanza con un futuro mejor para el país.
-            </h5>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-        </div>
+        <br>
+        <br>
+        <br>
+        <section id="home">
+            <div class="titulo">
+                <h1 style="text-align: left;">Medidas de tendencia central</h1> <br>
+                <h5>Las medidas de tendencia central son valores que determinan<br>el "centro" de los datos, es decir, el valor alrededor del cual tienden a acumularse.
+                </h5>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+            </div>
+        </section>
     </div>
     <section>
-        <section id="home">
+
+        <section id="media">
             <div class="row" style="padding: 20px;">
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card1-home.jfif" class="card-img-top" alt="Imagen 1">
+                        <img src="Imagenes/card1-media.jfif" class="card-img-top" alt="Imagen 1">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Milagro mexicano</h3>
-                            <p class="card-text" style="text-align: justify;">El milagro mexicano fue un periodo de crecimiento económico en Mexico, hubo un aumento anual del PIB de casi el 7%, esto gracias a la industrialización, la sustitución de importaciones y la estabilidad política entre los años (1940-1970)</p>
+                            <h3 class="card-title" style="text-align: center;">¿Qué es?</h3>
+                            <p class="card-text" style="text-align: justify;">La media es el promedio aritmético de los datos, se denota por x̄</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card2-home.jfif" class="card-img-top" alt="Imagen 2">
+                        <img src="Imagenes/card2-media.jfif" class="card-img-top" alt="Imagen 2">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Desigualdad</h3>
-                            <p class="card-text" style="text-align: justify;">La desigualdad del periodo giro entorno al crecimiento económico, mientras se construían grandes obras públicas algunos sectores vivían en condiciones de carencia lo que los hizo empezar a reclamar espacios de libertad y una mejor calidad de vida</p>
+                            <h3 class="card-title" style="text-align: center;">¿Cómo se calcula?</h3>
+                            <p class="card-text" style="text-align: justify;">La media se calcula dividiendo la suma total de valores entre el número total de valores </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card3-home.jfif" class="card-img-top" alt="Imagen 3">
+                        <img src="Imagenes/card3-media.jfif" class="card-img-top" alt="Imagen 3">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Marginados</h3>
-                            <p class="card-text" style="text-align: justify;">Los marginados del milagro mexicano fueron la base de toda la producción nacional. los campesinos contaban con tierras insuficientes, sometidos por cacicazgos y con escasa infraestructura, por otro lado, los obreros tenían salarios bajos, largas jornadas de trabajo y sindicatos bajo el control del corporativismo</p>
+                            <h3 class="card-title" style="text-align: center;">Ejemplo</h3>
+                            <p class="card-text" style="text-align: justify;">x̄=∑x/n<br>x̄=(17+17+18+21+23+24+24+24)/8<br>x̄=168/8<br>x̄=21</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section id="movimientos">
+        <section id="mediana">
             <div class="row" style="padding: 20px;">
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card1-movimientos.jfif" class="card-img-top" alt="Imagen 1">
+                        <img src="Imagenes/card1-mediana.jfif" class="card-img-top" alt="Imagen 1">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Movimiento campesino</h3>
-                            <p class="card-text" style="text-align: justify;">Los movimientos campesinos aparecieron por 3 tipos de problemáticas: económica, social y política.<br>Exigían tierras, justicia y apoyo social y económico.<br>El historiador Adolfo Gilly en "La revolución interrumpida(1971)" interpretaba los movimientos comoo una muestra de modernización desigual </p>
+                            <h3 class="card-title" style="text-align: center;">¿Qué es?</h3>
+                            <p class="card-text" style="text-align: justify;">La mediana es el valor medio de los datos ordenados de menor a mayor</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card2-movimientos.jfif" class="card-img-top" alt="Imagen 2">
+                        <img src="Imagenes/card2-mediana.jfif" class="card-img-top" alt="Imagen 2">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Movimientos obreros</h3>
-                            <p class="card-text" style="text-align: justify;">Hubo distintos movimientos de la clase obrera en los que destacan los siguientes :
-                            <ul style="justify-content: flex-start;">
-                                <li>Movimiento ferrocarrilero (1958-1959)</li>
-                                <li>Movimiento magisterial (1958)</li>
-                                <li>Movimiento médico (1964-1965)</li>
-                            </ul>
-                            </p>
-                            <spam boton><a href="https://www.youtube.com/watch?v=MQ8V8GKQmg4" class="btn btn-dark">Video informativo de los movimientos</a>
-                            </spam>
+                            <h3 class="card-title" style="text-align: center;">¿Cómo se calcula?</h3>
+                            <p class="card-text" style="text-align: justify;">Primero debes ordenar los datos de menor a mayor para identificar los valores de enmedio.<br>Se selecciona el valor que este en medio si el número total de datos es impar, si es par se seleccionan los 2 datos de enmedio y se calcula la media de estos datos.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card3-movimientos.jfif" class="card-img-top" alt="Imagen 3">
+                        <img src="Imagenes/card3-mediana.jfif" class="card-img-top" alt="Imagen 3">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Movimiento estudiantil</h3>
-                            <p class="card-text" style="text-align: justify;">Comenzó en julio de 1968, varios estudiantes formaron el Consejo Nacional de Huelga (CNH), demandaban libertad, indemnizaciones y reprimentas a los políticos.<br>El desenlace fue la matanza de Tlatelolco el 2 de octubre de 1968, en la plaza de las Tres Culturas.</p>
+                            <h3 class="card-title" style="text-align: center;">Ejemplo</h3>
+                            <p class="card-text" style="text-align: justify;">Datos ordenados de menor a mayor = 17,17,18,21,23,24,24,24<br>Mediana= (21+23)/2<br>Mediana= 22</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section id="protestas">
+        <section id="moda">
             <div class="row" style="padding: 20px;">
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card1-protestas.jfif" class="card-img-top" alt="Imagen 1">
+                        <img src="Imagenes/card1-moda.jfif" class="card-img-top" alt="Imagen 1">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Protestas económicas</h3>
-                            <p class="card-text" style="text-align: justify;">Muchos trabajadores y campesinos protestaban por salarios bajos, desigualdad en la distribución de la riqueza y condiciones laborales injustas.<br>A pesar del crecimiento industrial, los beneficios se concentraban en empresarios y sectores urbanos, dejando al campo rezagado.<br>El aumento del costo de vida afectaba a las familias, lo que generaba mayor inconformidad.</p>
+                            <h3 class="card-title" style="text-align: center;">¿Qué es?</h3>
+                            <p class="card-text" style="text-align: justify;">La moda es el valor que aparece con mayor frecuencia</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card2-protestas.jfif" class="card-img-top" alt="Imagen 2">
+                        <img src="Imagenes/card2-moda.jfif" class="card-img-top" alt="Imagen 2">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Protestas sociales</h3>
-                            <p class="card-text" style="text-align: justify;">Existía un fuerte descontento por la pobreza, la falta de servicios básicos (salud, educación, vivienda) y la desigualdad entre zonas urbanas y rurales.<br>Las clases populares no veían mejoras significativas en su calidad de vida.<br>También surgieron movimientos estudiantiles y sociales que exigían más oportunidades, igualdad y respeto a los derechos de la población.</p>
+                            <h3 class="card-title" style="text-align: center;">¿Cómo se calcula?</h3>
+                            <p class="card-text" style="text-align: justify;">Para calcularla de manera sencilla y rápida hacemos ua tabla de frecuencias de cada dato, siendo esto el dato por el número de veces que aparece dentro del ejercicio</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="Imagenes/card3-protestas.jfif" class="card-img-top" alt="Imagen 3">
+                        <img src="Imagenes/card3-moda.jfif" class="card-img-top" alt="Imagen 3">
                         <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Protestas políticas</h3>
-                            <p class="card-text" style="text-align: justify;">El sistema político estaba dominado por un solo partido, lo que limitaba la democracia, la participación ciudadana y la libertad de expresión.<br>Muchas protestas denunciaban la represión del gobierno hacia opositores y movimientos sociales.<br>Un ejemplo importante fue el movimiento estudiantil de 1968, que exigía libertades políticas y terminó siendo reprimido por el Estado.</p>
+                            <h3 class="card-title" style="text-align: center;">Ejemplo</h3>
+                            <p class="card-text" style="text-align: justify;">Datos: 17, 18, 21, 23, 24<br>Frecue: 02, 01, 01, 01, 03<br>Moda: 24 (se repite 3 veces, contando con la mayor frecuencia)</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section id="gobierno">
-            <div class="row" style="padding: 20px;">
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="Imagenes/card1-gobierno.jfif" class="card-img-top" alt="Imagen 1">
-                        <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Frases de orden</h3>
-                            <p class="card-text" style="text-align: justify;">
-                            <ul style="justify-content: flex-start;">
-                                <li>Díaz Ordaz: “El orden público es responsabilidad del Estado y debe mantenerse a toda costa.”</li>
-                                <li>Díaz Ordaz: “No permitiremos que intereses ajenos perturben la estabilidad del país.”</li>
-                                <li>López Mateos: “El respeto a la ley es base indispensable para la convivencia social.”</li>
-                            </ul>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="Imagenes/card2-gobierno.jfif" class="card-img-top" alt="Imagen 2">
-                        <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Frases de paz social</h3>
-                            <p class="card-text" style="text-align: justify;">
-                            <ul style="justify-content: flex-start;">
-                                <li>López Mateos: “La paz social se construye con justicia y bienestar para todos.”</li>
-                                <li>López Mateos: “Nuestro objetivo es que cada mexicano tenga acceso a mejores condiciones de vida.”</li>
-                                <li>Díaz Ordaz: “La tranquilidad de la nación es prioritaria frente a cualquier conflicto.”</li>
-                            </ul>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="Imagenes/card3-gobierno.jfif" class="card-img-top" alt="Imagen 3">
-                        <div class="card-body">
-                            <h3 class="card-title" style="text-align: center;">Frases de progreso</h3>
-                            <p class="card-text" style="text-align: justify;">
-                            <ul style="justify-content: flex-start;">
-                                <li>López Mateos: “El progreso de México debe traducirse en beneficios reales para el pueblo.”</li>
-                                <li>López Mateos: “La industrialización es el camino hacia el desarrollo nacional.”</li>
-                                <li>Díaz Ordaz: “El desarrollo económico requiere disciplina, trabajo y estabilidad.”</li>
-                            </ul>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <section id="referencias">
+            <div class="referencias">
+                <p>Taller de Probabilidad y Estadística II: Vol. Primera edición 2026 (Grupo Editorial Mx). (2025). César Iván Cobos May.</p>
             </div>
         </section>
     </section>
-    <div style="text-align: center; padding: 20px;">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/GdL6jIcqBtw" title="Video informativo de los movimientos" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </div>
 </body>
 
 </html>
